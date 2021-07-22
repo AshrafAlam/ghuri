20ebc8eed5e02f0ba2c0b2555209baeed4a35c2f
Ashraf Alam <joy_csharp@hotmail.com>
2021-07-22 12:41:54 +0100

    Initial commit

---
+++ data/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'EISK',
+        description: `Code and utilities to build scalable web applications easily.`,
+    imgSrc: '/static/images/eisk.png',
+        href: 'https://github.com/EISK',
+  },
+  {
+      title: 'SimpleCommands.NET',
+      description: `This program provides utilities and samples to help developers writing command based applications easily with following a test driven development approach.`,
+    imgSrc: '/static/images/time-machine.jpg',
+      href: 'https://github.com/AshrafAlam/SimpleCommands.NET',
+  },
+]
+
+export default projectsData
