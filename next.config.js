0c76e0491b05ad99c6fba38eb776d8bd6930f314
Ashraf Alam <joy_csharp@yahoo.com>
2022-02-05 05:29:10 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com *.disqus.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com joycsc.disqus.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
-  frame-src youtube.com www.youtube.com;
+  frame-src youtube.com www.youtube.com joycsc.disqus.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self' fonts.gstatic.com cdn.jsdelivr.net
+  font-src 'self' fonts.gstatic.com cdn.jsdelivr.net 
 `
 
 const securityHeaders = [
